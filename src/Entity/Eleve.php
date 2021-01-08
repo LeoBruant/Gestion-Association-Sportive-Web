8604ceaf9f4c51270403c3edbf8efad6b837dcfb
Leo Bruant <bruantleo@gmail.com>
2021-01-08 10:58:58 +0100

    ajout commentaires

--- src/Entity/Eleve.php
+++ src/Entity/Eleve.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface as UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=EleveRepository::class)
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"}, message="Il existe déjà un compte avec cet adresse email")
  */
 class Eleve implements UserInterface
 {
