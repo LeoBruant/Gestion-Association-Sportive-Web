cd24e5987121909be91922ef95e07042b8c36010
Maxence <maxou.mlyn@gmail.com>
2021-03-08 13:03:54 +0100

    add template form add event

--- src/Form/CreateEventType.php
+++ src/Form/CreateEventType.php
@@ -5,9 +5,11 @@ namespace App\Form;
 use App\Entity\Evenement;
 use App\Entity\Sport;
 use App\Entity\Categorie;
+use App\Entity\Type;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -19,23 +21,23 @@ class CreateEventType extends AbstractType
     {
         $builder
             ->add('nom', TextType::class)
-            ->add('description', TextType::class)
-            ->add('date', DateType::class)
+            ->add('description', TextareaType::class)
+            ->add('date', DateType::class, ['widget' => 'single_text', 'format' => 'yyyy-MM-dd'])
             ->add('nombre_places', TextType::class)
             ->add('image', FileType::class)
             ->add('vignette', FileType::class)
-            // ->add('sport', EntityType::class, [
-			// 	'class' => Sport::class,
-            //     'choice_label' => 'nom',
-			// ])
-            // ->add('type', EntityType::class, [
-			// 	'class' => Type::class,
-            //     'choice_label' => 'nom',
-			// ])
-            // ->add('categorie', EntityType::class, [
-			// 	'class' => Categorie::class,
-            //     'choice_label' => 'libelle',
-			// ])
+            ->add('sport', EntityType::class, [
+			    'class' => Sport::class,
+                'choice_label' => 'nom',
+			])
+            ->add('type', EntityType::class, [
+			'class' => Type::class,
+            'choice_label' => 'nom',
+			])
+            ->add('categorie', EntityType::class, [
+			    'class' => Categorie::class,
+                'choice_label' => 'libelle',
+			])
         ;
     }
 
