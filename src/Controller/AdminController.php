067d3af53c4f5bf1bfc4afe412de8af0910d782d
Tristan-Evenot <evenot.tristan@gmail.com>
2021-03-10 10:27:49 +0100

    Affiche liste des evenements

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -31,6 +31,7 @@ class AdminController extends AbstractController
      */
     public function newEvent(Request $request): Response
     {
+        $evenements = $this->getDoctrine()->getRepository(Evenement::class)->findAll();
         $event = new Evenement();
 
         $form = $this->createForm(CreateEventType::class, $event);
@@ -44,6 +45,7 @@ class AdminController extends AbstractController
 
 		return $this->render('admin/new-event.html.twig', [
             'event_form' => $form->createView(),
+            'evenements'=> $evenements,
         ]);
     }
 
@@ -130,4 +132,6 @@ class AdminController extends AbstractController
             'category_form' => $form->createView(),
         ]);
     }
+    
+
 }
